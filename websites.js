929b7ff6a1a7045be8368d5d966e070023b4cd41
Alexis Aguilar <alexmerona@gmail.com>
2022-07-09 15:06:10 -0400

    updating website objects

--- websites.js
+++ websites.js
@@ -1,14 +1,14 @@
 const websites = [
     {
         name: 'Get Waves',
-        image: ,
+        image: 'https://raw.githubusercontent.com/alexisintech/css-generator-api/assets/websiteimages/getwavesio.jpg',
         url: 'https://getwaves.io/',
         description: 'Create custom and unique SVG waves',
         keywords: ['waves', 'wave', 'svg', 'shape', 'shapes', 'svg shapes', 'svg waves'],
     },
     {
         name: 'uiverse',
-        image: ,
+        image: 'https://raw.githubusercontent.com/alexisintech/css-generator-api/assets/websiteimages/uiverseio.jpg',
         url: 'https://uiverse.io/',
         description: 'Choose from hundreds of design elements that are already coded for you! Just copy and paste!',
         keywords: ['button', 'buttons', 'checkbox', 'checkboxes', 'toggle', 'toggle switches', 'switches', 'cards', 'loaders', 'loading', 'loading icon', 'input', 'inputs'],
