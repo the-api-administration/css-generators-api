6016c23314d3dd3269bddee09ca5eb32444ded97
Alexis Aguilar <alexmerona@gmail.com>
2022-07-09 14:17:21 -0400

    creating the server

---
+++ resources.js
@@ -0,0 +1,16 @@
+const resources = [
+    {
+        name: 'Get Waves',
+        image: ,
+        url: 'https://getwaves.io/',
+        description: 'Create custom and unique SVG waves',
+        keywords: ['waves', 'wave', 'svg', 'shape', 'shapes', 'svg shapes', 'svg waves'],
+    },
+    {
+        name: 'uiverse',
+        image: ,
+        url: 'https://uiverse.io/',
+        description: 'Choose from hundreds of design elements that are already coded for you! Just copy and paste!',
+        keywords: ['button', 'buttons', 'checkbox', 'checkboxes', 'toggle', 'toggle switches', 'switches', 'cards', 'loaders', 'loading', 'loading icon', 'input', 'inputs'],
+    }
+]
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { resources } = require("./resources");
+const PORT = process.env.PORT || 8000;
+
+// =====================
+// Middleware
+// =====================
+app.set('view engine', 'ejs')
+app.use(cors());
+app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
+
+// =====================
+// Listen
+// =====================
+app.listen(PORT, () => {
+	console.log(`we live on port ${PORT}, bayyybeeeee 🔥`);
+});
+
+app.get('/', (req, res) => {
+	if (resources) {
+		res.render('index.ejs', { resources });
+	} else {
+		// respond with status 500 if the resources array could not be loaded from resources.js
+		res.status(500).json({
+			error: 'Resources were not able to be loaded from resources.js.'
+		});
+	}
+});
+
+app.get('/api', (req, res) => {
+	res.json(resources);
+});
+
+app.get('/api/:keyword', (req, res) => {
+	const keyword = req.params.keyword.toLowerCase();
+	
+	// filter resources array, return items that match query; tag.
+	const matches = resources.filter((obj) => obj.keywords.some(str => str.toLowerCase().includes(keyword)));
+
+	// if matches were found, respond with matches array in JSON format
+	if (matches.length) {
+		res.json(matches);
+	} else {
+		// respond with status 404, no matches were found
+		res.status(404).json({
+			error: `No resources were found with the ${keyword} keyword.`
+		});
+	}
+});
